a7c0d32056078afddcd60fcf376f55c352d56896
Mid_knight <62806659+Marx03@users.noreply.github.com>
2023-09-26 13:04:45 +0200

    Initiation

---
+++ Algo-js/apprentissage/exercices/exercice boucle/main.js
@@ -0,0 +1,5 @@
+var a=900
+
+for(let oklm=0 ; oklm<a ; oklm++ ) {
+    console.log("oklm")
+}
---
+++ Algo-js/apprentissage/exercices/exercice switch/main.js
@@ -0,0 +1,14 @@
+var a=4
+var b=4
+var c=3
+
+switch(a) {
+    case b:
+    console.log("égal à b")
+    
+    case c:
+    console.log("égal à c")
+
+    défault:
+    console.log("égal à rien")
+    }
